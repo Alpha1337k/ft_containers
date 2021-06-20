99f1738a3c5589d17c71a98a7bd7904cc264f25a
Alpha1337k <ozziekruithof@gmail.com>
2021-06-20 04:45:05 +0200

    Chore: removed old stack tests

--- main.cpp
+++ main.cpp
@@ -339,44 +339,7 @@ void	map_test(void)
 	std::cout << "empty: " << m.empty() << " size: " << m.size() << std::endl;
 }
 
-void	stack_test(void)
-{
-    stack<int> c1;
-    c1.push(5);
-    std::cout << c1.size() << '\n';
- 
-    stack<int> c2(c1);
-    std::cout << c2.size() << '\n';
- 
-    std::deque<int> deq {3, 1, 4, 1, 5};
-    stack<int> c3(deq);
-    std::cout << c3.size() << '\n';
-
-    stack<int>   s;
- 
-    s.push( 2 );
-    s.push( 6 );
-    s.push( 51 );
- 
-    std::cout << s.size() << " elements on stack\n";
-    std::cout << "Top element: "
-	      << s.top()         // Leaves element on stack
-	      << "\n";
-    std::cout << s.size() << " elements on stack\n";
-    s.pop();
-    std::cout << s.size() << " elements on stack\n";
-    std::cout << "Top element: " << s.top() << "\n";
-
-    stack<int> container;
- 
-    std::cout << "Initially, container.empty(): " << container.empty() << '\n';
- 
-    container.push(42);
-    std::cout << "After adding elements, container.empty(): " << container.empty() << '\n';
-    std::vector<std::string>
-        v1{"1","2","3","4"},
-        v2{"Ɐ","B","Ɔ","D","Ǝ"};
-}
+void stack_test();
 
 int main(void)
 {
