3ce23c985987a74e681d32c9cbd1401fb4993226
Alpha1337k <ozziekruithof@gmail.com>
2021-07-07 18:23:53 +0200

    Feat: enable  if

---
+++ src/ft_enable_if.hpp
@@ -0,0 +1,18 @@
+#ifndef FT_ENABLE_IF_HPP
+#define FT_ENABLE_IF_HPP
+
+namespace ft
+{
+
+	template<bool B, class T = void>
+	struct enable_if {};
+
+	template<class T>
+	struct enable_if<true, T> { typedef T type; };
+
+	template< bool B, class T = void >
+	using enable_if_t = typename enable_if<B,T>::type;
+
+}
+
+#endif
