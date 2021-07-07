64943c5c4b1b1fdb9fc003cbd781040648df0ef1
Alpha1337k <ozziekruithof@gmail.com>
2021-07-07 18:25:35 +0200

    Feat: enable if tests

---
+++ test/ft_enable_if.cpp
@@ -0,0 +1,32 @@
+#include <ft_enable_if.hpp>
+#include <type_traits>
+#include <iostream>
+#include <ft_is_integral.hpp>
+
+#ifdef USE_OS_VARIANT
+	using namespace std;
+#else
+	using namespace ft;
+#endif
+
+struct test
+{
+	enum { int_t, float_t } type;
+	template <typename Integer, enable_if_t<is_integral<Integer>::value, bool> = true >
+	test(Integer)
+	{
+		std::cout << "interger!!!" << std::endl;
+	}
+	template <typename Float, enable_if_t<std::is_floating_point<Float>::value, bool> = true >
+	test(Float)
+	{
+		std::cout << "float!!!" << std::endl;
+	}
+};
+
+
+void	enable_if_test()
+{
+	test(10);
+	test(10.10);
+}
