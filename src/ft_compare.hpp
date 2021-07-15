8e622df1cfe39e9ee6a65764cd03c4bd6493b1fb
Alpha1337k <ozziekruithof@gmail.com>
2021-07-15 08:30:26 +0200

    Feat: compare functions for the anal evaller

---
+++ src/ft_compare.hpp
@@ -0,0 +1,65 @@
+/* ._______________________________________________________________________. */
+/* |                                                                       | */
+/* |     ____  ______   | Oscar Kruithof                                   | */
+/* |    / __/ /__  __/  | https://github.com/Alpha1337k                    | */
+/* |   / /=     / /     | okruitho                                         | */
+/* |  /_/      /_/      |                                                  | */
+/* | _____________________________________________________________________ | */
+/* |       _                                                               | */
+/* |     /   )                                                             | */
+/* |    /       __    _   _/_   _    .   _     _     _      _              | */
+/* |   /      /   ) /   ) /   / - ) /  /   ) /  _) /   )  ( _ `            | */
+/* |  ( __ / ( _ / /   / /   /   / /  /   / (     /  \`  \ _ )             | */
+/* |                                                                       | */
+/* ._______________________________________________________________________. */
+
+#ifndef FT_COMPARE_HPP
+#define FT_COMPARE_HPP
+
+template< class InputIt1, class InputIt2 >
+bool lexicographical_compare( InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2 )
+{
+	for ( ; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2 ) {
+		if (*first1 < *first2) return true;
+        if (*first2 < *first1) return false;
+	}
+	return (first1 == last1) && (first2 != last2);	
+}
+
+template< class InputIt1, class InputIt2, class Compare >
+bool lexicographical_compare( InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Compare comp )
+{
+	for ( ; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2 ) {
+		if (comp(*first1, *first2)) return true;
+        if (comp(*first2, *first1)) return false;
+	}
+	return (first1 == last1) && (first2 != last2);	
+}
+
+template< class InputIt1, class InputIt2 >
+bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2 )
+{
+	while (first1 != last1)
+	{
+		if (*first1 != *first2)
+			return (false);
+		first1++;
+		first2++;
+	}
+	return (true);
+}
+
+template< class InputIt1, class InputIt2, class BinaryPredicate >
+bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p )
+{
+	while (first1 != last1)
+	{
+		if (p(*first1, *first2) == false)
+			return (false);
+		first1++;
+		first2++;
+	}
+	return (true);
+}
+
+#endif
